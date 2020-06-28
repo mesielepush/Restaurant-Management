48dbb2497ec01c84e31a2dbdd2dc9d10a3241bcb
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-06-27 21:58:11 -0500

    making sense of the dom manipulation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-import navbar from './nav_bar';
+const content = document.getElementById('content');
+    content.innerHTML = '';
+    const template = `
+                        <nav class="nav">
+                        <div class="container">
+                            <div class="logo">
+                                <a href="#">Your Logo</a>
+                            </div>
+                            <div id="mainListDiv" class="main_list">
+                                <ul class="navlinks">
+                                    <li><a href="#">About</a></li>
+                                    <li><a href="#">Portfolio</a></li>
+                                    <li><a href="#">Services</a></li>
+                                    <li><a href="#">Contact</a></li>
+                                </ul>
+                            </div>
+                            <span class="navTrigger">
+                                <i></i>
+                                <i></i>
+                                <i></i>
+                            </span>
+                        </div>
+                        </nav>
+                        <section class="home">
+                        </section>
+                        <div style="height: 1000px">
+                        
+                                
+                        </div>
+                        `;
+content.innerHTML += template;
+
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
