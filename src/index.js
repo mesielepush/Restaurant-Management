d0e2fc4a70071b4a8c6e8179a566471ebaead38f
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-06-29 13:53:14 -0500

    no more commented code

--- src/index.js
+++ src/index.js
@@ -4,25 +4,3 @@ import home from './home';
 
 navbar();
 home();
-
-
-/* window.scroll(function(){
-    if (document.scrollTop() > 50){
-        var logo_place = document.getElementsByClassName("logo");
-        var navb = document.getElementsByClassName('nav')
-        navb.addClass('back_nav');
-        logo_place.remooveClass('logo').addClass('logo_short')
-    } else{
-        var logo_place = document.getElementsByClassName("logo_short");
-        logo_place.remooveClass('logo_short').addClass('logo');
-        navb.remooveClass('back_nav');
-    }
-});
-var navTrigger = document.getElementsByClassName('navTrigger')
-navTrigger.click(function () {
-
-    this.toggleClass('active');
-    document.getElementById("mainListDiv").toggleClass("show_list");
-    document.getElementById("mainListDiv").fadeIn();
-
-}); */
